8053b199fa1ec881abfda2d6742bc826216836e2
lohith-dev <srlohith92@gmail.com>
2023-12-03 17:02:50 +0530

    Order page done from moongose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Order = require('../models/order');
 exports.getProducts = (req, res, next) => {
  
   Product.find()
@@ -125,38 +125,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
-    // .then(cart => {
-    //   fetchedCart = cart;
-    //   return cart.getProducts();
-    // })
-    // .then(products => {
-    //   return req.user
-    //     .createOrder()
-    //     .then(order => {
-    //       return order.addProducts(
-    //         products.map(product => {
-    //           product.orderItem = { quantity: product.cartItem.quantity };
-    //           return product;
-    //         })
-    //       );
-    //     })
-    //     .catch(err => console.log(err));
-    // })
-    // .then(result => {
-    //   return fetchedCart.setProducts(null);
-    // })
-    .then(result => {
-      res.redirect('/orders');
-    })
-    .catch(err => console.log(err));
+  .populate('cart.items.productId')
+  .then(user => {
+    const products = user.cart.items.map(i => {
+      return { quantity: i.quantity, product: { ...i.productId._doc } };
+    });
+    const order = new Order({
+      user: {
+        name: req.user.name,
+        userId: req.user
+      },
+      products: products
+    });
+    return order.save();
+  })
+  .then(result => {
+    return req.user.clearCart();
+  })
+  .then(() => {
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrder()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
    
       res.render('shop/orders', {
